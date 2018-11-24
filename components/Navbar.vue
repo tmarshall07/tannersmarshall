c9e5e5df28a036719fe93846bea89ec6a4e13647
Tanner Marshall <tanner.s.marshall@gmail.com>
2018-11-24 17:23:17 -0500

    First commit

---
+++ components/Navbar.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  .navbar-container
+    .navbar-list
+      .navbar-list-item(v-for="navItem in navItems") {{ navItem.name }}
+</template>
+
+<script>
+
+export default {
+  components: {
+
+  },
+  data () {
+    return {
+      navItems: [
+        { name: 'Home' },
+        { name: 'Comics' },
+        { name: 'Videos' },
+      ],
+    }
+  },
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+.navbar-list {
+  display: flex;
+
+  .navbar-list-item {
+    padding: 10px 15px;
+  }
+}
+
+</style>
---
+++ components/Template.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  div
+</template>
+
+<script>
+
+export default {
+  components: {
+
+  },
+  data () {
+    return {
+
+    }
+  },
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
