fcc6bcfea8178e2b0ca950ea067c621e3fd8789a
Tanner Marshall <tanner.s.marshall@gmail.com>
2019-01-26 09:47:10 -0500

    added january 25 comic

--- pages/comics/_comic.vue
+++ pages/comics/_comic.vue
@@ -36,6 +36,7 @@ export default {
       panelModalVisible: true,
       activePostId: 1,
       posts: [
+        { id: 14, imageId: 'game-night', name: 'Game night', date: '01/25/19', title: 'Just wait \'til we get to the section on trading!' },
         { id: 13, imageId: 'maybe-next-time', name: 'Maybe next time', date: '01/18/19', title: 'But from far away it seemed so small.' },
         { id: 12, imageId: 'wet-hands', name: 'Air drying', date: '01/11/19', title: 'I was just in the bathroom...It\'s water...from washing my hands. It\'s not pee.' },
         { id: 11, imageId: 'camping-creeps', name: 'Psst, you still awake?', date: '01/04/19', title: 'I\'ts all fun and games until the sun goes down.' },
