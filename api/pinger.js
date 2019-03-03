09376d230ad222b0c29bcbaf1096e9a3ecfcf965
Tanner Marshall <tanner.s.marshall@gmail.com>
2019-03-03 14:31:40 -0500

    Fixed pinger

--- api/pinger.js
+++ api/pinger.js
@@ -1,8 +1,13 @@
 // Ping the site every 5 minutes (300000 ms) to prevent Dyno from idling
 const http = require("http");
 
-export default function () {
+export default function (req, res, next) {
   setInterval(() => {
-      http.get("tannersmarshall.com");
+    http.get({
+      hostname: 'tannersmarshall.com',
+      path: '/',
+    });
   }, 300000);
+
+  next();
 }
