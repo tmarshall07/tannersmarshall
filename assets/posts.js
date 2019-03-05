6a9e0cc6db776f5d577057af2aca9b544c6f636b
Tanner Marshall <tanner.s.marshall@gmail.com>
2019-03-05 18:31:41 -0500

    Added yarn on accident

---
+++ assets/posts.js
@@ -0,0 +1,120 @@
+export const posts = [
+  {
+    id: 1,
+    imageId: 'nature-such-beauty',
+    name: 'Nature such beauty',
+    date: '10/23/18',
+    title: 'You should see what happens when it\'s a moth.',
+  },
+  {
+    id: 2,
+    imageId: 'name-forgetter-extraordinaire',
+    name: 'Name forgetter extraordinaire',
+    date: '10/30/18',
+    title: 'Hey...you! Good to see you again!'
+  },
+  {
+    id: 3,
+    imageId: 'guilt-trip-machine',
+    name: 'Guilt trip machine',
+    date: '11/06/18',
+    title: 'Don\'t judge me credit card machine.'
+  },
+  {
+    id: 4,
+    imageId: 'you-too',
+    name: 'You too!',
+    date: '11/13/18',
+    title: 'Do I...give the food back to him?'
+  },
+  {
+    id: 5,
+    imageId: 'octopus-foot',
+    name: 'Octopus foot',
+    date: '11/20/18',
+    title: 'Kat: "Hi, I\'m wondering if you have any bryophytes in stock?"\nPlant nursery guy: "Excuse me?"\nKat: "Like, moss?"\nPlant nursery guy:"Yes...we have moss."'
+  },
+  {
+    id: 6,
+    imageId: 'hawt-sauwce',
+    name: 'Hawt sauwce',
+    date: '11/27/18',
+    title: 'It\'s enjoyable, I swear!'
+  },
+  {
+    id: 7,
+    imageId: 'oh-technology',
+    name: 'Oh, technology',
+    date: '12/4/18',
+    title: 'Have you seen that app that lets you turn off all your apps?'
+  },
+  {
+    id: 8,
+    imageId: 'whats-for-breakfast',
+    name: 'But what\'s for breakfast?',
+    date: '12/13/18',
+    title: 'The sooner we go to bed, the sooner we can eat breakfast.'
+  },  {
+    id: 9,
+    imageId: 'all-the-flavors',
+    name: 'All the flavors',
+    date: '12/21/18',
+    title: 'Oh, and also a scoop of the peanut butter chocolate swirl please.'
+  },
+  {
+    id: 10,
+    imageId: 'middle-seats',
+    name: 'Middle seats',
+    date: '12/28/18',
+    title: 'Umm. Excuse me. You\'re elbow is in my ribcage. Thanks.'
+  },
+  {
+    id: 11,
+    imageId: 'camping-creeps',
+    name: 'Psst, you still awake?',
+    date: '01/04/19',
+    title: 'I\'ts all fun and games until the sun goes down.'
+  },
+  {
+    id: 12,
+    imageId: 'wet-hands',
+    name: 'Air drying',
+    date: '01/11/19',
+    title: 'I was just in the bathroom...It\'s water...from washing my hands. It\'s not pee.'
+  },
+  {
+    id: 13,
+    imageId: 'maybe-next-time',
+    name: 'Maybe next time',
+    date: '01/18/19',
+    title: 'But from far away it seemed so small.'
+  },
+  {
+    id: 14,
+    imageId: 'game-night',
+    name: 'Game night',
+    date: '01/25/19',
+    title: 'Just wait \'til we get to the section on trading!'
+  },
+  {
+    id: 15,
+    imageId: 'trail-runners',
+    name: 'Trail runners',
+    date: '02/02/19',
+    title: 'But like, how?'
+  },
+  {
+    id: 16,
+    imageId: 'convergent-evolution',
+    name: 'Convergent evolution',
+    date: '02/10/19',
+    title: 'Fine, if you get the bun then I get flannels.'
+  },
+  {
+    id: 17,
+    imageId: 'grocery-run',
+    name: 'Grocery run',
+    date: '03/03/19',
+    title: 'All right, we just need beans...and onions, oh let\'s get some garlic...Can\'t forget about milk!'
+  },
+];
---
+++ pages/archive.vue
@@ -0,0 +1,72 @@
+<template lang="pug">
+  .archive-page-wrapper
+    .posts-container
+      .post-container(v-for="post in posts", @click="goToComic(post.id)")
+        .post(:href="`/comics/${post.id}`")
+          span.post-title {{ post.name }}
+          span.post-date &nbsp;{{ post.date }}
+
+</template>
+
+<script>
+import { posts } from '~/assets/posts';
+import _ from 'underscore';
+
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+      posts: [],
+    }
+  },
+  created() {
+    this.posts = posts;
+  },
+  methods: {
+    goToComic(id) {
+      window.location.href = `/comics/${id}`;
+    },
+  }
+}
+</script>
+
+<style lang='scss', scoped>
+
+  .archive-page-wrapper {
+    display: flex;
+    justify-content: center;
+  }
+
+  .posts-container {
+    max-width: 350px;
+  }
+
+  .post-container {
+    font-size: 18px;
+    padding: 3px 8px;
+    border-radius: 3px;
+
+    cursor: pointer;
+
+    .post-date {
+      color: $light-grey-10;
+    }
+
+    &:hover, &:focus {
+      background: black;
+
+      color: white;
+
+      .post-date {
+        color: rgba(255, 255, 255, .6);
+      }
+    }
+  }
+
+  .link {
+    text-decoration: none;
+  }
+
+</style>
